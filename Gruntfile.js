878225ebddbc7b56fa52a8e72fb88c12006a3cef
Michael Matyus <michael@maty.us>
2014-08-03 19:52:17 -0400

    night time scene

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,14 @@ module.exports = function(grunt){
         files: [
           {'css/teeth_main.css': 'sass/teeth_main.scss'},
           {'css/bird_main.css': 'sass/bird_main.scss'},
+          {'css/night_main.css': 'sass/night_main.scss'},
           {'css/boat_main.css': 'sass/boat_main.scss'}
         ]
       }
     },
     autoprefixer: {
       no_dest: {
-        src: ['css/boat_main.css','css/teeth_main.css','css/bird_main.css']
+        src: ['css/boat_main.css','css/teeth_main.css','css/bird_main.css','css/night_main.css']
       }
     },
     watch: {
