e8ead3baed093807e130e6d4b6caa7f1a07a17dc
Michael Matyus <michael@maty.us>
2014-10-03 08:06:45 -0400

    more shit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt){
         },
         files: [
           {'css/teeth_main.css': 'sass/teeth_main.scss'},
+          {'css/flag_main.css': 'sass/flag_main.scss'},
           {'css/bird_main.css': 'sass/bird_main.scss'},
           {'css/night_main.css': 'sass/night_main.scss'},
           {'css/boat_main.css': 'sass/boat_main.scss'}
@@ -15,7 +16,13 @@ module.exports = function(grunt){
     },
     autoprefixer: {
       no_dest: {
-        src: ['css/boat_main.css','css/teeth_main.css','css/bird_main.css','css/night_main.css']
+        src: [
+          'css/boat_main.css',
+          'css/teeth_main.css',
+          'css/flag_main.css',
+          'css/bird_main.css',
+          'css/night_main.css'
+        ]
       }
     },
     watch: {
